9e1556cdd2811ce8a4a58a56c4d4d52558261320
Juan Dueñas <juan.duenas@stud.tu-darmstadt.de>
2024-01-07 22:59:40 +0100

    full functionality, final score still not calculated

--- Python/main.py
+++ Python/main.py
@@ -16,12 +16,13 @@ def print_state(rows, deck):
     for r in rows:
         print(r)
 
-def get_players_input(num_players, players_cards):
+def get_players_input(num_players, players_cards, players_point_cards):
     cards_played = []
     for i in range(0, num_players):
         print("Jugador", i+1, "¿Qué harás?")
         print(players_cards[i])
         print("[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]")
+        print("Cartas de puntos:", players_point_cards[i])
         player_input = ""
         is_valid_input = False
         index = 0
@@ -42,47 +43,94 @@ def get_players_input(num_players, players_cards):
 
 def get_players_cards(deck, num_players):
     players_cards = []
+    players_point_cards = []
     for i in range(0, num_players):
         players_cards.append([deck.pop()])
+        players_point_cards.append([])
     for i in range(1, 10):
         for j in range (0, num_players):
             players_cards[j].append(deck.pop())
-    return players_cards
+    for i in range(num_players):
+        players_cards[i].sort()
+    return players_cards, players_point_cards
 
-def process_turn(rows, players_selections):
-    # order players cards keeping track of the owner
+def get_player_row_selection(rows, player_id):
+    for row in rows:
+        print(row, get_card_points(row))
+    player_input = ""
+    invalid_input = True
+    while invalid_input:
+        player_input = input()
+        if player_input.isdigit():
+            index = int(player_input)   # está en [0,4)
+            if index >= 0 and index < 4:
+                invalid_input = False
+        else:
+            print("Please enter a number between 0 and 3")
+        pass
+    return int(player_input)
+
+def process_turn(rows, players_selections, players_point_cards):
+    sorted_list_indexes = sorted(range(len(players_selections)), key=lambda k: players_selections[k])
     # add them to the stacks
     # for card in range(len(players_selections)):
-    for card in players_selections:
+    for player_id in sorted_list_indexes:   # vamos a buscarle sitio a la carta de cada jugador en orden
         shortest_distance = 104
         closest_row = -1
-        for i in range(4):
-            distance = card-rows[i][-1]
-            # print("last card of row", i, "is", rows[i][-1])     # This seems to work just fine
-            if(distance > 0):   # la carta es mayor al último elemento de esa fila
-                # print("the card of the player,", card, "could go here, 'cause is higher")
-                if(distance < shortest_distance):
-                    # print("The distance is:", distance)
-                    closest_row = i
-                    shortest_distance = distance
-        if (closest_row==-1):   # la última carta de todas las filas es mayor a la carta jugada
-            print("No se pudo encontrar una fila para la carta :'c")
-            pass                # por ahora dejar así. Debería perder puntos ese jugador y se reinicia la fila
-        else:                   # se encontró dónde poner la carta
-            rows[closest_row].append(card)  # agregarla a la fila y se termina el ciclo, seguir con el siguiente jugador
-
+        for r in range(4):
+            distance = players_selections[player_id] - rows[r][-1]
+            if distance > 0:     # la carta es mayor y puede ir acá
+                if distance < shortest_distance:    # debe ir en esta fila
+                    closest_row = r                 # esta es la nueva fila más cercana
+                    shortest_distance = distance    # ahora debe superar esta distancia (ser más cercano)
+        if closest_row == -1:       # no se le encontró fila, hay que reiniciar una y sumar los puntos al jugador corresponidente
+            print("No hay fila para la carta", players_selections[player_id], ". El jugador", player_id+1, "pierde puntos.")
+            print("Jugador", player_id+1, "¿qué fila te quieres tragar?")
+            selected_row = get_player_row_selection(rows, player_id)
+            while len(rows[selected_row]) != 0:
+                players_point_cards[player_id].append(rows[selected_row].pop())
+            rows[selected_row].append(players_selections[player_id])
+        else:                       # Sí se le encontró fila, hay que agregarla
+            rows[closest_row].append(players_selections[player_id])  # agregarla a la fila y se termina el ciclo, seguir con el siguiente jugador
+            if len(rows[closest_row]) == 6:
+                print("La fila", closest_row+1, "debe ser procesada. El jugador", player_id+1, "se la come toda.")
+                for i in range(5):
+                    players_point_cards[player_id].append(rows[closest_row].pop(0))
     return
+
+def get_card_points(list_of_cards):
+    # # multiplos de 5 se llevan 3 puntos
+    # # multiplos de 10 se llevan 5 puntos
+    # # el 55 da 7 puntos
+    sum = 0
+    for card in list_of_cards:
+        sum += 1
+        if card == 55:
+            sum += 6
+        elif card %10 == 0:
+            sum += 4
+        elif card %5 == 0:
+            sum += 2
+    return sum
+
+
 
 rows, deck = start_game()
 running = True
 num_players = 3
-players_cards = get_players_cards(deck, num_players)
+players_cards, players_point_cards = get_players_cards(deck, num_players)
 
+# ## Test of points per card
+# list_test = list(range(1, 105))
+# for card in list_test:
+#     print("Carta", card, "gives", get_card_points([card]), "points")
+    
 
+# # run the game:
 for turn in range(0,10):
     print("Turn #", turn+1)
     print_state(rows, deck)
-    players_selections = get_players_input(num_players, players_cards)
-    process_turn(rows, players_selections)
+    players_selections = get_players_input(num_players, players_cards, players_point_cards)
+    process_turn(rows, players_selections, players_point_cards)
 
 print_state(rows, deck)
